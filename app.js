77a744d4411c0a536a69acfee1a64f546917df7a
Muhammad Mobeen <menosuper6@gmail.com>
2024-03-13 05:05:34 +0300

    hero sec updated

--- app.js
+++ app.js
@@ -5,32 +5,32 @@ const dropBtn = document.querySelector('.fa-x');
 const main = document.querySelector('main');
 const hero = document.querySelector('.hero');
 
-const images = ['./pics/hero.jpg', './pics/hero2.jpg', './pics/hero3.jpg', './pics/hero4.jpg' ];
+// const images = ['./pics/hero.jpg'];
 
-let currentIndex = 1;
+// let currentIndex = 1;
 
-function changeImage() {
-    hero.style.backgroundImage = `url('${images[currentIndex]}')`;
-    currentIndex = (currentIndex + 1) % images.length;
-}
+// function changeImage() {
+//     hero.style.backgroundImage = `url('${images[currentIndex]}')`;
+//     currentIndex = (currentIndex + 1) % images.length;
+// }
 
-// Change image every 5 seconds
-setInterval(changeImage, 4000);
+// // Change image every 5 seconds
+// setInterval(changeImage, 4000);
 
 
+
+
+// drop down menu functionality
 navBtn.addEventListener('click', function(){
     navDrop.classList.add('nav-off');
     main.classList.add('check');
     dropMenu.classList.add('drop-down');
 
 });
-
 dropBtn.addEventListener('click', function(){
     dropMenu.classList.remove('drop-down');
     main.classList.remove('check');
     navDrop.classList.remove('nav-off');
-    
-
 })
 
 
